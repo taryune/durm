da1d78447def9e4c6521528c5a56c73b07e48419
taryune <taryunelab@gmail.com>
2020-03-31 02:10:29 +0900

    add buttons

--- src/Events.svelte
+++ src/Events.svelte
@@ -1,16 +1,68 @@
 <script>
   import{ onMount } from "svelte";
+  import Button from 'svelma/src/components/Button.svelte'
   import Event from "./Event.svelte";
   let events;
+  let loading = false;
+  let inverted = "Week";
 
-  onMount(async () => {
-    await fetch(`https://script.google.com/macros/s/AKfycbyFRvliLoM3QYdEkF6LkiQzJnC41wCEim4iaB2XNZRXoJ3Ge6do/exec`)
+function changeArrStats(arr, target){
+  Object.keys(obj).forEach( function(key) {
+    if(key == target){ this.key =true} else {this.key=false};
+    });
+  }
+
+  async function loadEvents(start, end){
+    loading = true;
+    await fetch(`https://script.google.com/macros/s/AKfycbyFRvliLoM3QYdEkF6LkiQzJnC41wCEim4iaB2XNZRXoJ3Ge6do/exec?start=${start}&end=${end}`)
       .then(r => r.json())
       .then(data => {
         events = data;
+        loading = false;
+        if(start===0&&end===1){
+          inverted = "Today";
+        } else if(start===0&&end===7){
+          inverted = "Week";
+        }
       });
+  }
+
+  onMount(async () => {
+    await loadEvents(0,7);
   });
+
 </script>
+<div class="buttons">
+  {#if inverted == "Today"}
+    <Button type="is-light"
+            inverted
+            {loading}
+            on:click={() => loadEvents(0,1)}>
+            Today
+    </Button>
+  {:else}
+    <Button type="is-light"
+            {loading}
+            on:click={() => loadEvents(0,1)}>
+            Today
+    </Button>
+  {/if}
+  {#if inverted == "Week"}
+    <Button type="is-light"
+            inverted
+            {loading}
+            on:click={() => loadEvents(0,7)}>
+            Week
+    </Button>
+  {:else}
+    <Button type="is-light"
+            {loading}
+            on:click={() => loadEvents(0,7)}>
+            Week
+    </Button>
+  {/if}
+</div>
+
 
 <div class="columns is-ancestor is-multiline">
 {#if events}
@@ -19,8 +71,6 @@
           <Event {event}/>
       </div>
   {/each}
-{:else}
-    <div class="tile is-parent">loading</div>
 {/if}
 </div>
 
